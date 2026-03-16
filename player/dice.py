ca791f8975e16c0f467443a2d5c6f11eca40cbe1
Rebecca <rebeccaliria@gmail.com>
2026-03-16 15:31:34 +0100

    menu.py

--- player/dice.py
+++ player/dice.py
@@ -1,4 +1,15 @@
-# Rôle :
-# - Lancer les dés
-# - Retourner un nombre aléatoire
-# - Déterminer combien de joueurs peuvent passer
+import random
+
+# Lance un dé (1 à 6)
+def lancer_de():
+    return random.randint(1, 6)
+
+
+# Lance tous les dés des portes
+def lancer_des_portes(couleurs_portes):
+    resultats = {}
+
+    for couleur in couleurs_portes:
+        resultats[couleur] = lancer_de()
+
+    return resultats
